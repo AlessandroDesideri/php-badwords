ee3da9cfbcb4382a4637058ab341db2b138c177c
Alessandro <alessandro01desideri@gmail.com>
2022-02-14 16:21:42 +0100

    Stampa del testo

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+ $lorem = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique a iste reprehenderit assumenda molestiae atque labore natus commodi dolores. Molestias alias quae dolores beatae assumenda, aperiam quo delectus expedita repellendus.";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> <?php echo $lorem; ?> </h1>
+</body>
+</html>
